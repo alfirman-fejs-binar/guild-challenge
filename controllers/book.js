be771c27f4d7547172d200203fa0e0da46f97dd4
Kai <68959522+Hasyim-Kai@users.noreply.github.com>
2022-03-21 09:28:30 +0700

    refactor: book.js Controller

--- controllers/book.js
+++ controllers/book.js
@@ -5,9 +5,9 @@ const categoryPage = (req, res) => {
   res.render("category-page.ejs");
 };
 
-export const registerPage = (req, res) => {
+const registerPage = (req, res) => {
   const title = `Register`
   res.render("register-page", { title });
 };
 
-module.exports = { homPage, categoryPage };
+module.exports = { homPage, categoryPage, registerPage };
