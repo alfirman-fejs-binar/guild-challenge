18f49c3964813aaa699135d856a24a3807328060
Kai-69 <ichihatsu1@gmail.com>
2022-03-21 09:05:10 +0700

    feat : add Register Page

--- controllers/book.js
+++ controllers/book.js
@@ -5,4 +5,9 @@ const categoryPage = (req, res) => {
   res.render("category-page.ejs");
 };
 
+export const registerPage = (req, res) => {
+  const title = `Register`
+  res.render("register-page", { title });
+};
+
 module.exports = { homPage, categoryPage };
